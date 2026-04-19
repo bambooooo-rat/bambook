0d2b40e5ab550d98898cd1e73abf2ec91333c5bf
bambooooo-rat <bamboorat0528@gmail.com>
2026-04-19 20:16:48 +0800

    0419

--- course_data.js
+++ course_data.js
@@ -285,7 +285,7 @@ window.COURSE_DATA = {
             },
             {
                 "title": "handout6",
-                "blank": null,
+                "blank": "assets/工程數學/handout/handout6_填空版.pdf",
                 "sol": "assets/工程數學/handout/handout6_解答版.pdf"
             },
             {
